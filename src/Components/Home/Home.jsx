7a9f2976f88f4f550fabe64d81861c2382eb176a
Raj <rajsrivastav0603@gmail.com>
2023-01-09 11:51:08 +0530

    Navbar

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import "./Home.css"
+import Navbar from "../Navbar/Navbar"
+
+function Home(){
+
+    return(
+        <>
+        <div className="home-main">
+
+        <Navbar/>
+        </div>
+        </>
+    )
+}
+export default Home
